e04934b59bfa42a1d1c3abfa12c8f5eca553b1f5
MorskiBG <dymityrp@yahoo.com>
2021-11-22 22:40:13 +0200

    url moved to env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,20 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("http://localhost:3000/api/v1", {
-      target: "https://softuni-react-backend.herokuapp.com",
+    createProxyMiddleware(process.env.REACT_APP_BACKEND_URL, {
+      target: process.env.REACT_APP_FRONTEND_URL,
       changeOrigin: true,
     })
+    // createProxyMiddleware(
+    //   "https://softuni-react-power-app.herokuapp.com/api/v1",
+    //   {
+    //     target: "https://backend-react-powerapp.herokuapp.com",
+    //     changeOrigin: true,
+    //   }
+    // )
+    // createProxyMiddleware("http://localhost:3000/api/v1", {
+    //   target: "https://softuni-react-backend.herokuapp.com",
+    //   changeOrigin: true,
+    // })
   );
 };
