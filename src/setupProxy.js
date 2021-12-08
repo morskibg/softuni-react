3639b19c135f9b2ba7039c3897f3a7743ce0c626
MorskiBG <dymityrp@yahoo.com>
2021-12-08 20:38:19 +0200

    target refractorign

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  const target =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_SERVER_DEVELOPMENT
+      : process.env.REACT_APP_API_SERVER_PRODUCTION;
   app.use(
     createProxyMiddleware(process.env.API_VERSION, {
-      target:
-        !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-          ? process.env.REACT_APP_API_SERVER_DEVELOPMENT
-          : process.env.REACT_APP_API_SERVER_PRODUCTION,
+      target: target,
       changeOrigin: true,
     })
   );
