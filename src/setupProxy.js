1e27157be3f773b83b1ba3de1f14e9872aa033d5
MorskiBG <dymityrp@yahoo.com>
2021-12-08 11:42:30 +0200

    netlify deploy 1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  
   app.use(
-    // createProxyMiddleware(process.env.REACT_APP_FRONTEND_URL, {
-    //   target: process.env.REACT_APP_BACKEND_URL,
+    createProxyMiddleware(process.env.REACT_APP_FRONTEND_URL, {
+      target: process.env.REACT_APP_BACKEND_URL,
+      changeOrigin: true,
+    })
+    // createProxyMiddleware("https://softuni-react-powerapp.vercel.app", {
+    //   target: "https://backend-react-powerapp.herokuapp.com/api/v1",
     //   changeOrigin: true,
     // })
-    createProxyMiddleware(
-      "https://softuni-react-powerapp.vercel.app/",
-      {
-        target: "https://backend-react-powerapp.herokuapp.com/api/v1",
-        changeOrigin: true,
-      }
-    )
     // createProxyMiddleware("http://localhost:3000/api/v1", {
     //   target: "https://softuni-react-backend.herokuapp.com",
     //   changeOrigin: true,
