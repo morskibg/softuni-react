96c101c5d15a5afa1a3a8f6478f0f76f4d8954ff
MorskiBG <dymityrp@yahoo.com>
2021-12-08 15:34:14 +0200

    hardcoding proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,17 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(process.env.REACT_APP_FRONTEND_URL, {
-      target: process.env.REACT_APP_BACKEND_URL,
-      changeOrigin: true,
-    })
+    createProxyMiddleware(
+      "https://softuni-react-power-app.herokuapp.com/api/v1",
+      {
+        target: "https://backend-react-powerapp.herokuapp.com",
+        changeOrigin: true,
+      }
+    )
+    // createProxyMiddleware(process.env.REACT_APP_FRONTEND_URL, {
+    //   target: process.env.REACT_APP_BACKEND_URL,
+    //   changeOrigin: true,
+    // })
     // createProxyMiddleware("https://softuni-react-powerapp.vercel.app", {
     //   target: "https://backend-react-powerapp.herokuapp.com/api/v1",
     //   changeOrigin: true,
